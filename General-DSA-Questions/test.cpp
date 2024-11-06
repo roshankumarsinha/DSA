6d42571c1f399072dafee6f486611bee560ccf67
ROSHAN <roshan.sinha@cimpress.com>
2024-11-06 21:49:44 +0530

    Add HelpText

--- General-DSA-Questions/test.cpp
+++ General-DSA-Questions/test.cpp
@@ -1,15 +1,14 @@
 // Just to test some code
 
 #include <iostream>
-#include<queue>
+#include<set>
 using namespace std;
 
 int main() {
-    queue<int> q;
-    q.push(1);
-    q.push(2);
-    q.push(3);
-    q.push(4);
-    cout << q.front() << endl;
+    set<int> s;
+    s.insert(1);
+    s.insert(2);
+    s.insert(-1);
+    cout << s.count(1) << endl;
     return 0;
 }
