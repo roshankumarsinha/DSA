1c838e247120b0bf7c4b96bc0d20aff02a02db77
Roshan Kumar Sinha <roshankumarsinha@Roshans-MacBook-Air.local>
2026-02-02 00:09:34 +0530

    Done changes

--- General-DSA-Questions/Some Named Algo & Genral Ques/10_peakIndexInMountainArray.cpp
+++ General-DSA-Questions/Some Named Algo & Genral Ques/10_peakIndexInMountainArray.cpp
@@ -53,7 +53,7 @@ int main() {
 }
 
 
-// There is onr more modified version of this question which is :-
+// There is one more modified version of this question which is :-
 // Given an array of length N, peak element is defined as the element greater than both of its neighbors.
 // Formally, if arr[i] is the peak element, arr[i - 1] < arr[i] and arr[i + 1] < arr[i]. Find the index(0-based) of a peak element in the array.
 // If there are multiple peak numbers, return the index of any peak number. You may imagine that nums[-1] = nums[n] = -∞.
